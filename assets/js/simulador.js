6f86a86bb765e10eb66dfcbbb02a110057fe948e
brunocriacoes <br.rafael@outlook.com>
2021-11-08 10:55:15 -0300

    add memori

--- assets/js/simulador.js
+++ assets/js/simulador.js
@@ -1,18 +1,32 @@
 var app = new Vue({
     el: '#simulador',
     data: {
-        step: 1
+        step: 1,
+        planos: []
     },
     methods: {
         nex() {
             if( this.step < 6 ) {
                 ++this.step
             }
+            this.update()
         },
         prev() {
             if( this.step > 1 ) {
                 --this.step
             }
+            this.update()
+        },
+        update() {
+            let payload = {
+                step: this.step,
+                planos: this.planos
+            }
+            localStorage.setItem('simulador_tmp', JSON.stringify( payload ) )
         }
+
+    },
+    mounted() {
+        
     }
 })
